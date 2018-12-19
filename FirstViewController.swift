c5b019910c92ec1111b415b0018154916e9ae08b
Çağdaş Ekşi <cagdas.eksi@hotmail.com>
2018-12-19 23:15:44 +0300

    Initial commit.

---
+++ FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tabbar
+//
+//  Created by cagdas on 19.12.2018.
+//  Copyright © 2018 cagdas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbar
+//
+//  Created by cagdas on 19.12.2018.
+//  Copyright © 2018 cagdas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
